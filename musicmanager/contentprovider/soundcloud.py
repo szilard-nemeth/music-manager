23a0a3d9e9f6886a9a35c4c2931bbe2765d3395c
Szilard Nemeth <szilard.nemeth88@gmail.com>
2022-07-08 10:57:23 +0200

    SoundCloud provider: Accept URLs with 'soundcloud.app.goo.gl'

--- musicmanager/contentprovider/soundcloud.py
+++ musicmanager/contentprovider/soundcloud.py
@@ -5,20 +5,21 @@ from string_utils import auto_str
 from musicmanager.common import Duration
 from musicmanager.contentprovider.common import ContentProviderAbs
 
-SOUNDCLOUD_URL = "soundcloud.com"
+SOUNDCLOUD_NORMAL_URL = "soundcloud.com"
+SOUNDCLOUD_GOOGLE_URL = "soundcloud.app.goo.gl"
 
 
 @auto_str
 class SoundCloud(ContentProviderAbs):
     @classmethod
     def url_matchers(cls) -> Iterable[str]:
-        return [SOUNDCLOUD_URL]
+        return [SOUNDCLOUD_NORMAL_URL, SOUNDCLOUD_GOOGLE_URL]
 
     def is_media_provider(self):
         return True
 
     def can_handle_url(self, url):
-        if SOUNDCLOUD_URL in url:
+        if SOUNDCLOUD_NORMAL_URL in url or SOUNDCLOUD_GOOGLE_URL in url:
             return True
         return False
 
